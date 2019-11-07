8b1b0d3f474020c4ad4f5e808a30e13a02eb78d2
chieh-neu <wu.chie@husky.neu.edu>
2019-11-07 16:43:05 -0500

    kk

--- flask/hello_world.py
+++ flask/hello_world.py
@@ -4,6 +4,28 @@ from flask import Flask
 app = Flask(__name__)
 
 
+#import firebase_admin
+#from firebase_admin import credentials
+#from firebase_admin import firestore
+#
+#cred = credentials.Certificate('./static/json/pledgeforhumanity.json')
+#firebase_admin.initialize_app(cred)
+#db = firestore.client()
+#
+#
+#doc_ref = db.collection(u'users').document(u'abcde')
+#doc_ref.set({ u'first': u'Ada', u'last': u'Lovelace', u'born': 1815 })
+#
+#users_ref = db.collection(u'users')
+#for doc in users_ref.stream():
+#	print(u'{} => {}'.format(doc.id, doc.to_dict()))
+#
+#
+#import pdb; pdb.set_trace()
+
+
+
+
 
 @app.route('/<string:page_name>/')
 def render_static(page_name):
