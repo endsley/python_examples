fa08e284b80733e2be8225af644c5368499a6ffb
endsley <juliusctw@gmail.com>
2021-02-27 10:30:20 -0500

    updated string stuff

--- python_specific/string_manipulate.py
+++ python_specific/string_manipulate.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
+def pretty_np_array(m, front_tab):
+	out_str = front_tab + str(m).replace('\n ','\n' + front_tab).replace('[[','[').replace(']]',']') + '\n'
+	return out_str
 
 def block_string_concatenate(str1, str2, spacing='\t'):
-	L1 = str1.split('\n')
-	L2 = str2.split('\n')
+	L1 = str1.strip().split('\n')
+	L2 = str2.strip().split('\n')
+
+	if len(L1) > len(L2):
+		Δ = len(L1) - len(L2)
+		for ι in range(Δ):
+			L2.append('\n')
 
 	max_width = len(max(L1, key=len))
 	outS = ''
